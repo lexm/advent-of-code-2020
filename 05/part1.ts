36924fe37b2db3426daf1ce70a8dd46eefbb7d8c
“Lex <lexmyers8@gmail.com>
2020-12-12 17:25:00 -0800

    refactor of part 1

--- 05/part1.ts
+++ 05/part1.ts
@@ -2,17 +2,22 @@
 
 import { readFileSync } from 'fs'
 
+function parseSeatId(line: string) {
+    let seatId = line.split('').map((ch) => {
+        if(ch === 'F' || ch === 'L') return '0'
+        if(ch === 'B' || ch === 'R') return '1'
+    }).join('')
+    let seat = parseInt(seatId, 2)
+    if(isNaN(seat)) return 0
+    return seat
+}
+
 function parseData(arr: string[]) {
     let dict = {}
-    let count = 0
     let max = 0
     arr.forEach((line) => {
-        let seatId = line.split('').map((ch) => {
-            if(ch === 'F' || ch === 'L') return '0'
-            if(ch === 'B' || ch === 'R') return '1'
-        }).join('')
-        let seat = parseInt(seatId, 2)
-        if(!isNaN(seat) && seat > max) max = seat
+        let seat = parseSeatId(line)
+        if(seat > max) max = seat
     })
     return max
 }
